a92a35d88c6cdfd6844d700c6f9baf92e0ba813a
Andrey20001 <162813529+Andrey20001@users.noreply.github.com>
2025-11-15 17:39:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot, os, requests, random
 
-
 bot = telebot.TeleBot("СВОЙ ТОКЕН")
 
 @bot.message_handler(commands=['start'])
@@ -12,25 +11,36 @@ def send_mess(message):
 def send_hello_e(message):
     hello = (message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "Пластик кладут в ЖЕЛТЫЙ контейнер! Это 1 контейнер!")
+    with open('image/k1.png', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
+
 
 @bot.message_handler(commands=['пищевые_отходы'])
 def send_hello_e(message):
     hello = (message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "Пищевые отходы кладут в КОРИЧНЕВЫЙ контейнер! Это 2 контейнер!")
+    with open('image/k3.png', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
 
 @bot.message_handler(commands=['стекло'])
 def send_hello_e(message):
     hello = (message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "Стекло кладут в ЗЕЛЕНЫЙ контейнер! Это 3 контейнер!")
+    with open('image/k2.png', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
 
 @bot.message_handler(commands=['опасные_отходы'])
 def send_hello_e(message):
     hello = (message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "Опасные отходы например батарейки кладут в КРАСНЫЙ контейнер! Это 4 контейнер!")
+    with open('image/k4.png', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
 
 @bot.message_handler(commands=['бумага'])
 def send_hello_e(message):
     hello = (message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "Бумагу кладут в СИНИЙ контейнер! И это последний 5 контейнер!")
+    with open('image/k5.png', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
 
 bot.polling()
